a219ea5550a73a28ce1125122c60828b538f5d10
Mohamed <mohamed.sy@compscihigh.org>
2025-01-20 15:06:36 -0500

    created dynamic logo for the opportunity model

--- components/opportunities/opportunityCard/opportunityModel/modelHeader.jsx
+++ components/opportunities/opportunityCard/opportunityModel/modelHeader.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import work from "../assets/work.ico";
-
+import OpportunityLogo from "../opportunityLogo";
 
 const ModalHeader = (
     {
@@ -17,7 +17,7 @@ const ModalHeader = (
 
     return (
         <div className="bg-white text-black flex place-items-center gap-7 pb-4">
-            <Image src={work} alt="work logo" className="size-9" />
+            <OpportunityLogo type={opportunityType[0]} />
             <h1 className="font-extrabold max-w-[15em] text-xl">{opportunityName}</h1>
         </div>
     )
