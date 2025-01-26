e9da3ae918d45d64c0b3f3cc4e2d0f52f6578fbe
Zachary Dorcinville <zdorcinville@gmail.com>
2025-01-26 17:06:48 -0500

    rebase against main

--- app/talent/partners/page.js
+++ app/talent/partners/page.js
@@ -39,9 +39,9 @@ export default function Partners() {
   const [isPartnerModalOpen, setPartnerModalOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [partnerTypes, setPartnerTypes] = useState([]);
-  const [selectedPartnerType, setSelectedPartnerType] = useState('');
+  const [selectedPartnerType, setSelectedPartnerType] = useState("");
   const [partnersInvolved, setInvolved] = useState([]);
-  const [selectedInvolvement, setSelectedInvolvement] = useState('');
+  const [selectedInvolvement, setSelectedInvolvement] = useState("");
   const onOpenModal = () => setPartnerModalOpen(true);
   const onCloseModal = () => setPartnerModalOpen(false);
 
@@ -64,16 +64,22 @@ export default function Partners() {
           const involvedSet = new Set();
           for (const p of data.records) {
             if (!partnerTypesSet.has(p.fields["Partner Type"])) {
-              partnerTypesSet.add(p.fields['Partner Type']); 
-            } if (!involvedSet.has(p.fields["Is Involved in CodeHouse"])) {
-              involvedSet.add(p.fields['Is Involved in CodeHouse']);
+              partnerTypesSet.add(p.fields["Partner Type"]);
+            }
+            if (!involvedSet.has(p.fields["Is Involved in CodeHouse"])) {
+              involvedSet.add(p.fields["Is Involved in CodeHouse"]);
             }
           }
           // console.log("SET:", involvedSet);
 
-          setPartnerTypes(Array.from(partnerTypesSet).filter((p) => !!p && p !== "undefined" && p !== undefined).map((p) => ({label: p, value: p})));
-          setInvolved(Array.from(involvedSet).map((p) =>({label: p, value: p})));
-
+          setPartnerTypes(
+            Array.from(partnerTypesSet)
+              .filter((p) => !!p && p !== "undefined" && p !== undefined)
+              .map((p) => ({ label: p, value: p }))
+          );
+          setInvolved(
+            Array.from(involvedSet).map((p) => ({ label: p, value: p }))
+          );
         }
       } catch (e) {
         console.error(e);
@@ -87,82 +93,108 @@ export default function Partners() {
     onOpenModal();
   };
 
-  const filteredPartners = partners.filter((p) => {
-    if (searchQuery.length > 0 && p.fields["Partner Name"]?.includes(searchQuery)) return true;
-    if (p.fields["Partner Type"] === selectedPartnerType) return true;
-    if (p.fields["Is Involved in CodeHouse"]?.includes(partnersInvolved)) return true;
-  })
-
   const getFilteredPartners = () => {
     let newPartners = [...partners];
-    if(searchQuery.length > 0) {
+    if (searchQuery.length > 0) {
       newPartners = newPartners.filter((p) => {
         return p.fields["Partner Name"]?.includes(searchQuery);
-      })
-    } 
+      });
+    }
     if (selectedPartnerType.length > 0) {
       newPartners = newPartners.filter((p) => {
-        console.log("test", selectedPartnerType, p.fields["Partner Type"])
-        return p.fields["Partner Type"]?.toLowerCase() === selectedPartnerType.toLowerCase();
-      })
+        console.log("test", selectedPartnerType, p.fields["Partner Type"]);
+        return (
+          p.fields["Partner Type"]?.toLowerCase() ===
+          selectedPartnerType.toLowerCase()
+        );
+      });
     }
     if (selectedInvolvement.length > 0) {
       newPartners = newPartners.filter((p) => {
-        return p.fields["Is Involved in CodeHouse"]?.toLowerCase() === selectedInvolvement.toLowerCase();
-      })
+        return (
+          p.fields["Is Involved in CodeHouse"]?.toLowerCase() ===
+          selectedInvolvement.toLowerCase()
+        );
+      });
     }
     return newPartners;
-  }
+  };
 
-  console.log(getFilteredPartners());
   return (
     <Flex
       textAlign="center"
       align="center"
-      border="2px solid red"
       minH="100vh"
       justify="center"
       w="full"
       direction="column"
-      p={3}
+      pt="5rem"
+      px="4rem"
+      gap={8}
     >
       <section>
-        <h1>Our Partners</h1>
-        <h2>Learn more about top companies looking for top talent</h2>
-        <Input placeholder="Search by Name" onChange={(e) => setSearchQuery(e.currentTarget.value)}/>
-        <Flex gap="40px">
+        <div className="flex flex-col justify-center items-center w-full">
+          <Heading size="5xl" fontWeight="bold" pt="6">
+            Our Partners
+          </Heading>
+          <Heading
+            size="4xl"
+            fontWeight="light"
+            textAlign="center"
+            className="w-[70%]"
+            pb="4"
+          >
+            Learn more about top companies looking for top talent
+          </Heading>
+        </div>
+        <div className="flex flex-col gap-4 justify-center items-center w-full">
+          <Input
+            placeholder="Search by Name"
+            onChange={(e) => setSearchQuery(e.currentTarget.value)}
+          />
+          <Flex gap="40px">
+            <SelectRoot
+              collection={createListCollection({ items: partnerTypes })}
+              size="sm"
+              width="320px"
+              onValueChange={(e) => setSelectedPartnerType(e.value[0])}
+            >
+              <SelectLabel>Partner Type</SelectLabel>
+              <SelectTrigger>
+                <SelectValueText placeholder="Any" />
+              </SelectTrigger>
+              <SelectContent>
+                {partnerTypes.map((type, i) => (
+                  <SelectItem item={type} key={type.value}>
+                    {type.label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </SelectRoot>
 
-          <SelectRoot collection={createListCollection({items: partnerTypes})} size="sm" width="320px" onValueChange={(e) => setSelectedPartnerType(e.value[0])}>
-            <SelectLabel>Partner Type</SelectLabel>
-            <SelectTrigger>
-              <SelectValueText placeholder="Any"/>
-            </SelectTrigger>
-            <SelectContent>
-              {partnerTypes.map((type, i) => (
-                <SelectItem item={type} key={type.value} >
-                  {type.label}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </SelectRoot>
-
-          <SelectRoot collection={createListCollection({items: partnersInvolved})} size="sm" width="320px" onValueChange={(e) => setSelectedInvolvement(e.value[0])}>
-            <SelectLabel>Involved in CodeHouse Programs</SelectLabel>
-            <SelectTrigger>
-              <SelectValueText placeholder="Yes" />
-            </SelectTrigger>
-            <SelectContent>
-              {partnersInvolved.map((program, i) => (
-                <SelectItem item={program} key={i} value={program.value}>
-                  {program.label}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </SelectRoot>
-        </Flex>
+            <SelectRoot
+              collection={createListCollection({ items: partnersInvolved })}
+              size="sm"
+              width="320px"
+              onValueChange={(e) => setSelectedInvolvement(e.value[0])}
+            >
+              <SelectLabel>Involved in CodeHouse Programs</SelectLabel>
+              <SelectTrigger>
+                <SelectValueText placeholder="Yes" />
+              </SelectTrigger>
+              <SelectContent>
+                {partnersInvolved.map((program, i) => (
+                  <SelectItem item={program} key={i} value={program.value}>
+                    {program.label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </SelectRoot>
+          </Flex>
+        </div>
       </section>
       <PartnersGrid
-        partners={partners}
+        partners={getFilteredPartners()}
         currentPage={currentPage}
         setCurrentPage={setCurrentPage}
         itemsPerPage={itemsPerPage}
